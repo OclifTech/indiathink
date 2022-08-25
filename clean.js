3e82245d718a8a70e991f38c67246441558359d0
Adarsh Hatkar <80952515+AdarshHatkar@users.noreply.github.com>
2022-08-25 18:28:10 +0530

    initial setup with bootstrap

---
+++ clean.js
@@ -0,0 +1,26 @@
+import { existsSync, lstatSync, readdirSync, unlinkSync, rmdirSync } from 'fs';
+
+function deleteFolderRecursive(path) {
+  if (existsSync(path) && lstatSync(path).isDirectory()) {
+    readdirSync(path).forEach(function(file, index){
+      index;
+      var curPath = path + '/' + file;
+
+      if (lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        unlinkSync(curPath);
+      }
+    });
+
+    console.log(`Deleting directory "${path}"...`);
+    rmdirSync(path);
+  }
+}
+
+console.log('Cleaning working tree...');
+
+deleteFolderRecursive('./dist');
+deleteFolderRecursive('.parcel-cache');
+
+console.log('Successfully cleaned working tree!');
